33b12dd9ae8b3d336cf92fa98d0c61081fe81d51
Henrique Sanfilippo Takahashi <henrique.sanfilippo@gmail.com>
2025-03-11 09:29:28 -0300

    feat fotos dos produtos

--- src/components/CardItem/index.tsx
+++ src/components/CardItem/index.tsx
@@ -1,27 +1,57 @@
 const items = [
   {
     id: 1,
-    name: "Notebook Dell",
-    description: "Notebook Dell i7, 16GB RAM, 512GB SSD.",
-    image: "https://placehold.co/200",
+    name: "TV 40 Polegadas",
+    description: "Em bom estado, nenhum defeito na imagem",
+    image: "https://static-file-vault.vercel.app/fotos-doacao/tv.jpeg",
   },
   {
     id: 2,
-    name: "Monitor LG",
-    description: "Monitor LG 24'' Full HD.",
-    image: "https://placehold.co/200",
+    name: "Mesa em L",
+    description: "Mesa em L separável em 2 mesas comuns.",
+    image: "https://static-file-vault.vercel.app/fotos-doacao/mesas_pc.jpeg",
   },
   {
     id: 3,
-    name: "Cadeira Gamer",
-    description: "Cadeira gamer ergonômica, cor preta e vermelha.",
-    image: "https://placehold.co/200",
+    name: "Mesa de centro",
+    description: "Mesa de centro com espaço para armazenamento interno",
+    image: "https://static-file-vault.vercel.app/fotos-doacao/mesa_centro.jpeg",
   },
   {
     id: 4,
-    name: "Fone de Ouvido JBL",
-    description: "Fone Bluetooth JBL com cancelamento de ruído.",
-    image: "https://placehold.co/200",
+    name: "Halteres (10kg cada)",
+    description: "Halteres modulares para musculação.",
+    image: "https://static-file-vault.vercel.app/fotos-doacao/halteres.jpeg",
+  },
+  {
+    id: 5,
+    name: "Fogão elétrico",
+    description: "Fogão de indução 2 bocas, até 1400w ou 700w nas duas juntas.",
+    image: "https://static-file-vault.vercel.app/fotos-doacao/fogao.jpeg",
+  },
+  {
+    id: 6,
+    name: "Espelho de mesa",
+    description: "Espelho de mesa oval.",
+    image: "https://static-file-vault.vercel.app/fotos-doacao/espelho.jpeg",
+  },
+  {
+    id: 7,
+    name: "Cadeira de escritório",
+    description: "Cadeira de escritório com apoio para os braços.",
+    image: "https://static-file-vault.vercel.app/fotos-doacao/cadeira.jpeg",
+  },
+  {
+    id: 8,
+    name: "Armário de cozinha",
+    description: "Armário com 2 tomadas escondidas",
+    image: "https://static-file-vault.vercel.app/fotos-doacao/armario.jpeg",
+  },
+  {
+    id: 9,
+    name: "Forno com microondas",
+    description: "Forno, microondas e grill. Em bom estado, funcionando normal.",
+    image: "https://static-file-vault.vercel.app/fotos-doacao/forno.jpeg",
   },
 ]
 
@@ -29,7 +59,7 @@ const CardItem = () => {
     return (
         <div className="container mt-7 pt-7 text-light bg-dark min-vh-100 p-4">
             <h1 className="text-center mb-7 mt-7">Doação</h1>
-            <div className="p-5">
+            <div className="p-1">
                 <div className="row">
                     {items.map((item) => (
                         <div key={item.id} className="col-12 col-md-6 mb-4">
